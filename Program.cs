ac33fdf45d769128736ae8accf9b6f02bef199b5
Hafsat Dere <derehafsat24@gmail.com>
2026-04-13 01:17:59 +0100

    assignment

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+var desktopPath = @"C:\Users\Hafasat\Desktop";
+var filePath = $"{desktopPath}\\MGQSFile.txt";
+var docxPath = $"{desktopPath}\\MGQSFile.docx";
+var folderPath = $"{desktopPath}\\MGQSFolder";
+var copiedFolder = $"{folderPath}\\MGQSFile.txt";
+Directory.CreateDirectory(folderPath);
+string fullName = "Dere Hafsat Omowunmi";
+//File.WriteAllLines(filePath,Enumerable.Repeat(fullName,10));
+for (int i = 0 ; i < 10 ; i ++)
+{
+    File.AppendAllText(filePath,fullName + Environment.NewLine);
+}
+File.Copy(filePath,copiedFolder,true);
+string content = File.ReadAllText(filePath);
+File.WriteAllText(docxPath,content);
+Directory.Delete(folderPath);
+
+
